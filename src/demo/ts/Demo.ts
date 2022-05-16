72db07a35139c5889f862f465881d965853c73fb
baibonjwa <baibonjwa@gmail.com>
2022-05-16 09:54:26 +0800

    Init plugin

--- src/demo/ts/Demo.ts
+++ src/demo/ts/Demo.ts
@@ -1,13 +1,16 @@
-import { TinyMCE } from 'tinymce';
+import { TinyMCE } from "tinymce";
 
-import Plugin from '../../main/ts/Plugin';
+import Plugin from "../../main/ts/Plugin";
 
 declare let tinymce: TinyMCE;
 
 Plugin();
 
 tinymce.init({
-  selector: 'textarea.tinymce',
-  plugins: 'code ghostban-tinymce-plugin',
-  toolbar: 'ghostban-tinymce-plugin'
+  selector: "textarea.tinymce",
+  plugins: "code ghostban-tinymce-plugin",
+  toolbar: "ghostban-tinymce-plugin",
+  valid_elements: "iframe[!src|width|height|frameborder:0|allowfullscreen]",
+  extended_valid_elements:
+    "iframe[src|frameborder|style|scrolling|class|width|height|name|align]",
 });
--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -1,16 +1,93 @@
-import { Editor, TinyMCE } from 'tinymce';
+import { Editor, TinyMCE } from "tinymce";
 
 declare const tinymce: TinyMCE;
 
 const setup = (editor: Editor, url: string): void => {
-  editor.ui.registry.addButton('ghostban-tinymce-plugin', {
-    text: 'ghostban-tinymce-plugin button',
+  editor.ui.registry.addButton("ghostban-tinymce-plugin", {
+    text: "Go",
     onAction: () => {
-      editor.setContent('<p>content added from ghostban-tinymce-plugin</p>');
-    }
+      // editor.setContent(
+      //   "<p>hhh`【content added from ghostban-tinymce-plugin</p>"
+      // );
+      return editor.windowManager.open({
+        title: "Embed Go/Weiqi/Baduk",
+        body: {
+          type: "panel",
+          items: [
+            {
+              type: "input",
+              name: "share_url",
+              label: "Share URL(required)",
+            },
+            {
+              type: "listbox", // component type
+              name: "type", // identifier
+              label: "Board Type", // text for the label
+              items: [
+                { text: "Simple", value: "si" },
+                { text: "Standard", value: "st" },
+                { text: "Advanced", value: "ad" },
+              ],
+            },
+            {
+              type: "input",
+              name: "title",
+              label:
+                "Title(optional, only visible in standard and advanced mode)",
+            },
+            {
+              type: "htmlpanel", // A HTML panel component
+              html: "",
+            },
+          ],
+        },
+        buttons: [
+          {
+            type: "cancel",
+            text: "Close",
+          },
+          {
+            type: "submit",
+            text: "Insert",
+            primary: true,
+          },
+        ],
+        onSubmit: function (api) {
+          const data: any = api.getData();
+          /* Insert content when the window form is submitted */
+          // editor.insertContent(`<p>${data.share_url}<p>`);
+          // editor.insertContent(`<iframe src="${data.share_url}" />`);
+          let url = data.share_url;
+          let width = 320;
+          let height = 350;
+          if (data.title) {
+            url += `&t=${data.title}`;
+          }
+
+          if (data.type) {
+            url += `&k=${data.type}`;
+          }
+
+          if (data.type === "st" || data.type === "ad") {
+            width = 640;
+            height = 360;
+          }
+
+          if (data.type === "ad") {
+            width = 680;
+            height = 360;
+          }
+
+          editor.insertContent(
+            `<div><iframe frameborder="0" scrolling="no" width="${width}" height="${height}" src="${url}"></iframe></div>`
+          );
+          api.close();
+        },
+      });
+    },
   });
 };
 
 export default (): void => {
-  tinymce.PluginManager.add('ghostban-tinymce-plugin', setup);
+  tinymce.PluginManager.add("ghostban-tinymce-plugin", setup);
 };
