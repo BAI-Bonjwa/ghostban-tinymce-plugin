913256f0205b1ff97cbd2c7eb89bc8d295381a12
baibonjwa <baibonjwa@gmail.com>
2022-05-18 16:52:48 +0800

    Add lang directory

---
+++ langs/zh_CN.js
@@ -0,0 +1,10 @@
+tinymce.addI18n("zh_CN", {
+  "Board Type": "棋盘类型",
+  Simple: "简洁",
+  Basic: "基本",
+  Standard: "标准",
+  "Embed Go/Weiqi/Baduk": "嵌入棋盘",
+  "Share URL(required)": "分享 URL（必填）",
+  "Title(optional, only visible in standard and advanced mode)":
+    "标题（可选，仅标准模式可见）",
+});
